f2c1a2d75d3e1770e10df473309eace25eabf21f
JustinSoper <jsoper1@student.mtsac.edu>
2026-05-23 21:28:21 -0700

    Added my obstacle avoidance function
    The following variables need to be added
    Detection
    bool oneoclk_dtc,  bool threeoclk_dtc,  bool fiveoclk_dtc,
    bool sevenoclk_dtc,  bool nineoclk_dtc,  bool tenoclk_dtc
    //Distance
    float oneoclk_dist,  float threeoclk_dist,  float fiveoclk_dist
    float sevenoclk_dist,  float nineoclk_dist,  float tenoclk_dist
    Turn Speeds
    right_turnspd=0;
    left_turnspd=0;
    
    //Turn Speed Constant (found through trial and error)
    int k

--- Code/SlavePilot/Motors/Movement.cpp
+++ Code/SlavePilot/Motors/Movement.cpp
@@ -18,7 +18,23 @@ void reverse(unsigned long durationMs) {
   stopMotors();
 }
 
-void turnRight(unsigned long durationMs) {
+// This might have an issue where the speeds continuosly decrease as new obstacles are found without returning to the original speed
+/*
+
+if (oneoclk_dtc == True || threeoclk_dtc == True || nineoclk_dtc == True || tenoclk_dtc == True || 
+(fiveoclk_dtc == True && fiveoclk_dist < 50) || (sevenoclk_dtc ==True && sevenoclk_dist < 50)
+{
+right_Turnspd= K*((1/oneoclk_dist) + (1/threeoclk_dist) + (1/fiveoclk_dist))
+left_TurnSpd = K*((1/sevenoclk_dist) + (1/nineoclk_dist) + (1/tenoclk_dist))
+RIGHT_DRIVE_SPEED = RIGHT_DRIVE_SPEED - left_Turnspd
+RIGHT_DRIVE_SPEED = RIGHT_DRIVE_SPEED - right_Turnspd
+}
+
+*/
+
+
+
+/*void turnRight(unsigned long durationMs) {
   setLeftTrack(true, TURN_SPEED);
   setRightTrack(false, TURN_SPEED);
   delay(durationMs);
@@ -31,3 +47,4 @@ void turnLeft(unsigned long durationMs) {
   delay(durationMs);
   stopMotors();
 }
+*/
